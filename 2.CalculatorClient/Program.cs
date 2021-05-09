aab58fc3ee842e0331310c022c41eca6baa1b4c8
Sergio Navarro Pino <sergio.navarropino@gmail.com>
2021-05-09 16:42:28 +0200

    Adding projects

---
+++ 2.CalculatorClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Serginet.Calculator;
+
+namespace CalculatorClient
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var a = 5;
+            var b = 7;
+            var calculator = new Calculator();
+
+
+            Console.WriteLine($"a = {a}, b = {b}");
+            Console.WriteLine($"a + b = {calculator.Add(a,b)}");
+            Console.WriteLine($"a - b = {calculator.Subtract(a, b)}");
+            Console.WriteLine($"a * b = {calculator.Multiply(a, b)}");
+            Console.WriteLine($"a / b = {calculator.Divide(a, b)}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nuget-private-feed-posts
-Repository to support blog articles
+
+Repository to support blog articles (in spanish) related to use privage NuGet feeds in Azuree DevOps
+
+1. [Consumiendo paquetes nuget privados: Experiencia de desarrollo + CI/CD](https://www.serginet.com/2021/05/consumiendo-paquetes-nuget-privados.html)
